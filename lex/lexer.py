e58ff70ee4ea73bfef7c0e4efbb510dc2030c65f
ywmaggie <zhyuwei0115@gmail.com>
2013-11-20 21:03:26 -0800

    a more detailed lexer plus FILE input and output

--- lex/lexer.py
+++ lex/lexer.py
@@ -7,41 +7,83 @@ def C(n,m):
 tokens = (
     "BEGIN",
     "END",
-    "ANSWER",
     "ANSWER_BLANK",
-    "MATH_EXPRESSION",
+    "ANSWER_BEGIN",
+    "ANSWER_END",
+    "MATH_LPAREN",
+    "MATH_RPAREN",
     "BOLD_MARK",
-    "WORDS",
+    "NUMBER",
+    "PARAMETER",
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
+    "LPAREN",
+    "RPAREN",
+    "EQUAL",
+    "DOUBLE_QUOTATION_MARK",
+    "SINGLE_QUOTATION_MARK",
+    "WORD",
     "NEWLINE",
-    "BLANK"
+    "BLANK",
+    "COMMENT",
+    "PUNCTUATION"
+    
 )
-#ignore the start part, which we don't need in XML files
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_EQUAL = r'='
+t_DOUBLE_QUOTATION_MARK = r'\"'
+t_SINGLE_QUOTATION_MARK = r'\''
+
 def t_BEGIN(t):
-    r"[\s\S]*BEGIN_PGML"
+    r"[\s\S]*\$showPartialCorrectAnswers\ =\ 1\;"
     pass
+
+def t_BEGIN_PG(t):
+    r"BEGIN_PGML"
+    pass
+    
+def t_COMMENT(t):
+    r"\#\s*"
+    pass
+
+#ignore the start part, which we don't need in XML files
+
 
 #ignore the ending part
 def t_END(t):
     r"END_PGML[\s\S]*"
     pass
 
-#to get the true answer out if the format of "Compute"
-def t_ANSWER(t):
-    r"\{Compute\(\".+\"\)\}"
-    split_1 = t.value
-    split_2 = split_1.split('{Compute(\"')[1]
-    split_3 = split_2.split('\")}')[0]	
-    t.value = eval(split_3)
-    return t
-
 #find the place to fill in answers
 def t_ANSWER_BLANK(t):
     r"\[_+\]"
     return t
 
-#find the math expression different from words that need to be carefully dealt with
-def t_MATH_EXPRESSION(t):
-    r"\[\`\\\w+\`\]"
+#where the expected answer begins
+def t_ANSWER_BEGIN(t):
+    r"\{Compute\(\""
+    return t
+
+#where the expected answer ends
+def t_ANSWER_END(t):
+    r"\"\)\}"
+    return t
+
+#the typical parentheses for a math expression
+def t_MATH_LPAREN(t):
+    r"\[\`"
+    return t
+
+def t_MATH_RPAREN(t):
+    r"\`\]"
     return t
 
 #find the mark of bold content
@@ -49,9 +91,22 @@ def t_BOLD_MARK(t):
     r"\*"
     return t
 
-#the normal words,with punctuation
-def t_WORDS(t):
-    r"[\w\'\",\.\?-]+[ \t]*"
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+def t_PARAMETER(t):
+    r'$\w'
+    return t
+
+#the normal words,without punctuation
+def t_WORD(t):
+    r"[\\a-zA-Z_-]+"
+    return t
+
+def t_PUNCTUATION(t):
+    r"[,.?]+"
     return t
 
 #a new line
@@ -62,7 +117,6 @@ def t_NEWLINE(t):
 #the other blanks we want to ignore
 def t_BLANK(t):
     r"\s+"
-    pass
 
 #error handling
 def t_error(t):
